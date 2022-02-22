abc94eb1f0d9677dc15862ae073ec2efbe4f8b86
Christopher Reimann <christopher.reimann@leebmann.de>
2022-02-22 08:52:02 +0100

    format

--- mj.js
+++ mj.js
@@ -1,19 +1,19 @@
 require('dotenv').config()
 
-const mailjet = require ('node-mailjet')
-.connect(process.env.MJ_APIKEY_PUBLIC, process.env.MJ_APIKEY_PRIVATE);
+const mailjet = require('node-mailjet')
+    .connect(process.env.MJ_APIKEY_PUBLIC, process.env.MJ_APIKEY_PRIVATE);
 
 let request;
 
-switch(process.argv[2]) {
-    case "createContact": 
+switch (process.argv[2]) {
+    case "createContact":
         //Create a Contact
         request = mailjet
-            .post("contact", {'version': 'v3'})
+            .post("contact", { 'version': 'v3' })
             .request({
-            "IsExcludedFromCampaigns":"true",
-            "Email": process.argv[3],
-            "Name": process.argv[4]
+                "IsExcludedFromCampaigns": "true",
+                "Email": process.argv[3],
+                "Name": process.argv[4]
             })
         request
             .then((result) => {
@@ -24,10 +24,10 @@ switch(process.argv[2]) {
             })
         break;
 
-    case "createList": 
+    case "createList":
         //Create a Contactlist
         request = mailjet
-            .post("contactslist", {'version': 'v3'})
+            .post("contactslist", { 'version': 'v3' })
             .request({
                 "Name": process.argv[3]
             })
@@ -37,183 +37,186 @@ switch(process.argv[2]) {
             })
             .catch((err) => {
                 console.log(err.statusCode)
-        })
+            })
         break;
 
     case "addContact":
         //Add a contact to a list
-         request = mailjet
-        .post("listrecipient", {'version': 'v3'})
-        .request({
-            "ContactAlt": process.argv[3],
-            "ListID": process.argv[4]
-        })
+        request = mailjet
+            .post("listrecipient", { 'version': 'v3' })
+            .request({
+                "ContactAlt": process.argv[3],
+                "ListID": process.argv[4]
+            })
         request
             .then((result) => {
-            console.log(result.body)
-            //Remove possible exclusion
-            request = mailjet
-            .put("contact", {'version': 'v3'})
-            .id(process.argv[3])
-            .request({
-                "IsExcludedFromCampaigns":"false",
-            })
-            request
-            .then((result) => {
                 console.log(result.body)
+                //Remove possible exclusion
+                request = mailjet
+                    .put("contact", { 'version': 'v3' })
+                    .id(process.argv[3])
+                    .request({
+                        "IsExcludedFromCampaigns": "false",
+                    })
+                request
+                    .then((result) => {
+                        console.log(result.body)
+                    })
+                    .catch((err) => {
+                        console.log(err.statusCode)
+                    })
             })
             .catch((err) => {
                 console.log(err.statusCode)
             })
-        })
-        .catch((err) => {
-            console.log(err.statusCode)
-        })
-        .catch((err) => {
-            console.log(err.statusCode)
-        })
+            .catch((err) => {
+                console.log(err.statusCode)
+            })
         break;
 
     case "unsubContact":
         //Unsubscribe contact
         request = mailjet
-        .post("contactslist", {'version': 'v3'})
-        .id(process.argv[4])
-        .action("managecontact")
-        .request({
-            "Action":"unsub",
-            "Email": process.argv[3]
-        })
-        request
-        .then((result) => {
-            console.log(result.body)
-        })
-        .catch((err) => {
-            console.log(err.statusCode)
-        })
-        break;
-        
-    case "subContact":
-        //Subscribe contact
-        request = mailjet
-        .post("contactslist", {'version': 'v3'})
-        .id(process.argv[4])
-        .action("managecontact")
-        .request({
-            "Action":"addForce",
-            "Email": process.argv[3]
-        })
-        request
-        .then((result) => {
-             console.log(result.body)
-            //Remove possible exclusion
-            request = mailjet
-            .put("contact", {'version': 'v3'})
-            .id(process.argv[3])
+            .post("contactslist", { 'version': 'v3' })
+            .id(process.argv[4])
+            .action("managecontact")
             .request({
-                "IsExcludedFromCampaigns":"false",
+                "Action": "unsub",
+                "Email": process.argv[3]
             })
-            request
+        request
             .then((result) => {
                 console.log(result.body)
             })
             .catch((err) => {
                 console.log(err.statusCode)
             })
-        })
-        .catch((err) => {
-            console.log(err.statusCode)
-        })
+        break;
+
+    case "subContact":
+        //Subscribe contact
+        request = mailjet
+            .post("contactslist", { 'version': 'v3' })
+            .id(process.argv[4])
+            .action("managecontact")
+            .request({
+                "Action": "addForce",
+                "Email": process.argv[3]
+            })
+        request
+            .then((result) => {
+                console.log(result.body)
+                //Remove possible exclusion
+                request = mailjet
+                    .put("contact", { 'version': 'v3' })
+                    .id(process.argv[3])
+                    .request({
+                        "IsExcludedFromCampaigns": "false",
+                    })
+                request
+                    .then((result) => {
+                        console.log(result.body)
+                    })
+                    .catch((err) => {
+                        console.log(err.statusCode)
+                    })
+            })
+            .catch((err) => {
+                console.log(err.statusCode)
+            })
         break;
 
     case "excludeContact":
         //Exclude contact from all compaigns
         request = mailjet
-        .put("contact", {'version': 'v3'})
-        .id(process.argv[3])
-        .request({
-            "IsExcludedFromCampaigns":"true",
-        })
+            .put("contact", { 'version': 'v3' })
+            .id(process.argv[3])
+            .request({
+                "IsExcludedFromCampaigns": "true",
+            })
         request
-        .then((result) => {
-            console.log(result.body)
-        })
-        .catch((err) => {
-            console.log(err.statusCode)
-        })
+            .then((result) => {
+                console.log(result.body)
+            })
+            .catch((err) => {
+                console.log(err.statusCode)
+            })
         break;
 
     case "includeContact":
         //include contact in compaigns
         request = mailjet
-        .put("contact", {'version': 'v3'})
-        .id(process.argv[3])
-        .request({
-            "IsExcludedFromCampaigns":"false",
-        })
+            .put("contact", { 'version': 'v3' })
+            .id(process.argv[3])
+            .request({
+                "IsExcludedFromCampaigns": "false",
+            })
         request
-        .then((result) => {
-            console.log(result.body)
-        })
-        .catch((err) => {
-            console.log(err.statusCode)
-        })
+            .then((result) => {
+                console.log(result.body)
+            })
+            .catch((err) => {
+                console.log(err.statusCode)
+            })
         break;
-        
-    case "showContacts": 
+
+    case "showContacts":
         //Show a list of all contacts
         request = mailjet
-        .get("contact", {'version': 'v3'})
-        .request()
+            .get("contact", { 'version': 'v3' })
+            .request()
         request
-        .then((result) => {
-            console.log(result.body)
-        })
-        .catch((err) => {
-            console.log(err.statusCode)
-        })
+            .then((result) => {
+                console.log(result.body)
+            })
+            .catch((err) => {
+                console.log(err.statusCode)
+            })
         break;
 
     case "showContact":
+        //Show a specific contact
         request = mailjet
-        .get("contact", {'version': 'v3'})
-        .id(process.argv[3])
-        .request()
+            .get("contact", { 'version': 'v3' })
+            .id(process.argv[3])
+            .request()
         request
-        .then((result) => {
-            console.log(result.body)
-        })
-        .catch((err) => {
-            console.log(err.statusCode)
-        })
+            .then((result) => {
+                console.log(result.body)
+            })
+            .catch((err) => {
+                console.log(err.statusCode)
+            })
         break;
 
-    case "showList": 
+    case "showList":
         //Show a list of all contacts in a list
         request = mailjet
-        .get("contact", {'version': 'v3'})
-        .request({"ContactsList": process.argv[3]})
+            .get("contact", { 'version': 'v3' })
+            .request({ "ContactsList": process.argv[3] })
         request
-        .then((result) => {
-            console.log(result.body)
-        })
-        .catch((err) => {
-            console.log(err.statusCode)
-        })
+            .then((result) => {
+                console.log(result.body)
+            })
+            .catch((err) => {
+                console.log(err.statusCode)
+            })
         break;
+
     case "showLists":
         //Show a list of all lists
         request = mailjet
-        .get("contactslist", {'version': 'v3'})
-        .request()
+            .get("contactslist", { 'version': 'v3' })
+            .request()
         request
-        .then((result) => {
-            console.log(result.body)
-        })
-        .catch((err) => {
-            console.log(err.statusCode)
-        })
+            .then((result) => {
+                console.log(result.body)
+            })
+            .catch((err) => {
+                console.log(err.statusCode)
+            })
         break;
+
     default:
         console.log(`
 
